f358e4aebbeb56590bf618f8a0fd1f2d6c08c805
Anurag <anuragmy2729@gmail.com>
2019-03-03 11:21:10 +0530

    Update ManageProfilesActivity.java

--- app/src/main/java/com/example/sihtry1/ManageProfilesActivity.java
+++ app/src/main/java/com/example/sihtry1/ManageProfilesActivity.java
@@ -1,13 +1,85 @@
 package com.example.sihtry1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.sihtry1.models.NRC;
+import com.example.sihtry1.models.RCR;
+import com.example.sihtry1.models.Referral;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class ManageProfilesActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference profileref = db.collection("referral");
+
+    private ManageProfilesAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_profiles);
+
+        setupRecyclerView();
+        adapter.setOnItemClickListener(new ManageProfilesAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                Referral profile = documentSnapshot.toObject(Referral.class);
+                String id = documentSnapshot.getId();
+
+
+            }
+        });
+
     }
+
+    public void setupRecyclerView() {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        Query query = profileref.orderBy("referral_id", Query.Direction.ASCENDING);
+
+
+        FirestoreRecyclerOptions<Referral> options = new FirestoreRecyclerOptions.Builder<Referral>()
+                .setQuery(query, Referral.class)
+                .build();
+
+
+
+        adapter = new ManageProfilesAdapter(options);
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewmanage);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
 }
+
